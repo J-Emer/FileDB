ca9786da575eda0a3b5e17f5fe5b47e69a05d21c
PartumGames <jemerson6989@gmail.com>
2024-12-15 17:35:02 -0600

    initial commit
    setting up the project

---
+++ src/PathHelper.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace Jemer\Phphelpers;
+
+
+class PathHelper
+{
+
+    public static function DirectoryName(string $path) : string 
+    {
+        return pathinfo($path)['dirname'];
+    }
+    public static function NamewithExtension(string $path) : string 
+    {
+        return pathinfo($path)['basename'];
+    }
+    public static function NamewithoutExtension(string $path) : string 
+    {
+        return pathinfo($path)['filename'];
+    }
+    public static function Extension(string $path) : string 
+    {
+        return pathinfo($path)['extension'];
+    }    
+    public static function BuildPath(array $arr) : string
+    {
+        $str = "";
+
+        foreach ($arr as $key) 
+        {
+            $str .= $key . DIRECTORY_SEPARATOR;
+        }
+
+        return rtrim($str, DIRECTORY_SEPARATOR);
+    }
+}
+
+?>
